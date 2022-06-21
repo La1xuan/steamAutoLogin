c7ebeb2da3af39f36446193012b1c03a11e0540f
La1xuan <bilibililaixuan@gmail.com>
2022-06-21 10:15:28 -0400

    Add modifying feature

--- steamUpload.py
+++ steamUpload.py
@@ -3,6 +3,13 @@ import json
 import pyautogui
 
 
+def finalize():
+    temp = json.dumps(loginInfo)
+    f = open("LoginInfo.json","w+")
+    f.write(temp)
+    f.close()
+    quit()
+
 try:
     f = open("LoginInfo.json","r")
     temp = f.read(99999)
@@ -10,20 +17,40 @@ try:
     f.close()
 except:
     loginInfo = [[],[]]
-loginInfo[0].append("Add")
+loginInfo[0].append("Modifying")
 res = pyautogui.confirm(text="Which to Login?", buttons=loginInfo[0])
-if res == "Add":
-    loginInfo[0][-1] = pyautogui.prompt(text="Username")
-    loginInfo[1].append(pyautogui.prompt(text="Password"))
-    temp = json.dumps(loginInfo)
-    
-    f = open("LoginInfo.json","w+")
-    f.write(temp)
-    f.close()
-    exit()
+if res == "Modifying":
+    res = pyautogui.confirm(text="Choose your move", buttons=["Add", "Change Username", "Change Password", "Delete Account"])
+    if res == "Add":
+        loginInfo[0][-1] = pyautogui.prompt(text="Username")
+        loginInfo[1].append(pyautogui.prompt(text="Password"))
+        finalize()
+    if res == "Change Username":
+        loginInfo[0].pop()
+        res = pyautogui.confirm(text="Which account to change?", buttons=loginInfo[0])
+        ind = loginInfo[0].index(res)
+
+        loginInfo[0][ind] = pyautogui.prompt(text="new username")
+        finalize()
+    if res == "Change Password":
+        loginInfo[0].pop()
+        res = pyautogui.confirm(text="Which account to change?", buttons=loginInfo[0])
+        ind = loginInfo[0].index(res)
+
+        loginInfo[1][ind] = pyautogui.prompt(text="new password")
+        finalize()
+    if res == "Delete Account":
+        loginInfo[0].pop()
+        res = pyautogui.confirm(text="Which account to delete?", buttons=loginInfo[0])
+        ind = loginInfo[0].index(res)
+
+        loginInfo[0].pop(ind)
+        loginInfo[1].pop(ind)
+        finalize()
 
 f.close()
 ind = loginInfo[0].index(res)
-os.popen("steam.exe -login " + loginInfo[0][ind] + " " + loginInfo[1][ind])
-exit()
+print("steam.exe -login " + loginInfo[0][ind] + " " + loginInfo[1][ind])
+#os.popen("steam.exe -login " + loginInfo[0][ind] + " " + loginInfo[1][ind])
+quit()
 
